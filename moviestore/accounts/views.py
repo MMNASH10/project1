315914a573bc8f9deb796ff8ca530953550bdad8
Cyrus <cyong34@gatech.edu>
2025-02-04 17:37:18 -0500

    added custom signup form

--- moviestore/accounts/views.py
+++ moviestore/accounts/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from .forms import CustomUserCreationForm, CustomErrorList
+from django.contrib.auth import login as auth_login, authenticate
 
-from django.contrib.auth.forms import UserCreationForm
 
 
 def signup(request):
     template_data = {}
     template_data["title"] = "Sign Up"
     if request.method == "GET":
-        template_data["form"] = UserCreationForm()
+        template_data["form"] = CustomUserCreationForm()
         return render(request, "accounts/signup.html", {"template_data": template_data})
+    elif request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home.index')
+        else:
+            template_data['form'] = form
+            return render(request, 'accounts/signup.html',
+                {'template_data': template_data})
