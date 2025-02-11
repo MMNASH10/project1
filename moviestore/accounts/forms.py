21f2dc62740af929d0273f2d6c761c380fbd9848
ctlh1202 <cle45@student.gsu.edu>
2025-02-11 18:12:49 -0500

    added some comments (no change in functionality)

--- moviestore/accounts/forms.py
+++ moviestore/accounts/forms.py
@@ -10,7 +10,8 @@ class CustomErrorList(ErrorList):
             return ''
         return mark_safe(''.join([
             f'<div class="alert alert-danger" role="alert">{e}</div>' for e in self]))
-        
+
+#source: chatgpt
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(
         required=True,
@@ -29,7 +30,6 @@ class CustomUserCreationForm(UserCreationForm):
             self.fields[fieldname].widget.attrs.update({"class": "form-control"})
 
     def clean_email(self):
-        """Ensure email is unique"""
         email = self.cleaned_data.get("email")
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("A user with this email already exists.")
